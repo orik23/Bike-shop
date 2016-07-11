c58c66498d2527ec0a9541046a97c2a9c0598f86
Marta <martafarreribas@gmail.com>
2016-07-11 15:29:12 +0200

    added color field to products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -42,6 +42,6 @@ class ProductsController < ApplicationController
     end
 
     def product_params
-      params.require(:product).permit(:name, :description, :image_url)
+      params.require(:product).permit(:name, :description, :image_url, :colour)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160706122405) do
+ActiveRecord::Schema.define(version: 20160708100043) do
 
   create_table "comments", force: true do |t|
     t.integer  "user_id"
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(version: 20160706122405) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.decimal  "price"
-    t.string   "color"
+    t.string   "colour"
   end
 
   create_table "users", force: true do |t|
