0d2a55e0c1def437914fbcf3a9fd25a6847a17ee
Marta <martafarreribas@gmail.com>
2016-07-15 16:21:21 +0200

    5.1

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -22,8 +22,15 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.new(product_params)
-    @product.save
-    respond_with(@product)
+    respond_to do |format|
+      if @product.save
+        format.html { redirect_to @product, notice: 'Product was successfully created.' }
+        format.json { render :show, status: :created, location: @product }
+      else
+        format.html { render :new }
+        format.json { render json: @product.errors, status :unprocessable_entity }
+      end
+    end
   end
 
   def update
