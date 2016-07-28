18b840562f4fc7f4b1bdfede8df23d91cb11cdd1
Marta <martafarreribas@gmail.com>
2016-07-28 14:48:49 +0200

    5

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -33,7 +33,7 @@ class ProductsController < ApplicationController
         format.json { render :show, status: :created, location: @product }
       else
         format.html { render :new }
-        format.json { render json: @product.errors, status :unprocessable_entity }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
       end
     end
   end
