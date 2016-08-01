b20ac5bc35780d01228b78cc5817bd4d879361ff
Marta <martafarreribas@gmail.com>
2016-08-01 15:23:19 +0200

    6.2

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
-RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Product do
+	when the product has comments do
+	end 
+
+	before do
+		@product = Product.create!(name: "race bike")
+		@user = User.create!(email: hello@hello.com, password: 1234567a)
+		@product.comments.create!(rating: 1, user: @user, body: "Awful bike!")
+		@product.comments.create!(rating: 3, user: @user, body: "It is okay")	
+		@product.comments.create!(rating: 5, user: @user, body: "The best bike!")
+	end
+
+	it "should return average rating" do
+		expect(product.average_rating). to eq 3
+	end
+
+	it "is not valid" do
+		expect(Product.new(description: "Nice bike!")).not_to be_valid
+	end
+
 end
+
+
+
+
+
