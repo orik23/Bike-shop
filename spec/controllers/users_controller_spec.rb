83a76c212d1a9385d4ad94a79c917eeecda79f5a
Marta <martafarreribas@gmail.com>
2016-08-01 18:43:26 +0200

    6.3

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,38 +1,37 @@
 require 'rails_helper'
 
 describe UsersController, :type => :controller do
-
-  before do
-    @user = User.create!(first_name: "Andy", last_name: "Lindeman", email: "andy@example.com", password: "abcdefgh")
-    @user2 = User.create!(first_name: "Andy", last_name: "Lindeman", email: "andy2@example.com", password: "abcdefgh")
-    #@user = FactoryGirl.create(:user)
-  end
-
   describe "GET #show" do
-     context "User is logged in" do 
-       before do
-         sign_in @user
-       end
+    before do
+      @user = User.create!(first_name: "Andy", last_name: "Lindeman", email: "andy@example.com", password: "abcdefgh")
+      @user2 = User.create!(first_name: "Andy", last_name: "Lindeman", email: "andy2@example.com", password: "abcdefgh")
+      #@user = FactoryGirl.create(:user)
+    end
 
-       it "loads the correct user details" do
-         get :show, id: @user.id
-         expect(response.status).to eq 200
-         expect(assigns(:user)).to eq @user
-       end
-
-       it "doesn't load the second user" do
-         get :show, id: @user2.id
-         expect(response.status).to eq 302
-         expect(response).to redirect_to(root_path)
-       end
+    context "User is logged in" do 
+      before do
+      sign_in @user
      end
 
-     context "No user is logged in" do
-       it "redirects to login" do
-         get :show, id: @user.id
-         expect(response).to redirect_to(root_path)
-       end
+    it "loads the correct user details" do
+      get :show, id: @user.id
+      expect(response.status).to eq 200
+      expect(assigns(:user)).to eq @user
+    end
+
+    it "doesn't load the second user" do
+      get :show, id: @user2.id
+      expect(response.status).to eq 302
+      expect(response).to redirect_to(root_path)
      end
+   end
+
+    context "No user is logged in" do
+     it "redirects to login" do
+       get :show, id: @user.id
+       expect(response).to redirect_to(root_path)
+      end
+    end
   end
 
 end
