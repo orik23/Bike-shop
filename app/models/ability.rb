0dbcd9d7bc3e15205d6f43e02068d2e997fb578d
Marta <martafarreribas@gmail.com>
2016-08-04 16:25:25 +0200

    debugging errors

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,8 +2,12 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new # guest user (not logged in)
-    can :manage, User, id: user.id
+  	if user.admin?
+  		can :manage, Comment
+  		can :manage, User
+  	else
+  		can [:read, :create], Comment
+  		can :manage, User, id: user.id
+  	end
   end
-
 end
