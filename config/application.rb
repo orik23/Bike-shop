1e9e5cfbefc28bc7edb89479d23eb44be26e65a6
Marta <martafarreribas@gmail.com>
2016-08-08 16:10:04 +0200

    6.9

--- config/application.rb
+++ config/application.rb
@@ -6,8 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.force_ssl = true
-
 module CfShop
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -21,5 +19,7 @@ module CfShop
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.force_ssl = true
+
   end
 end
