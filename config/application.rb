ab8a0428fb35ea544f2751108f6b9178fe455b79
Marta <martafarreribas@gmail.com>
2016-08-08 16:52:36 +0200

    rails s does not work

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,5 @@ module CfShop
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.force_ssl = true
-
   end
 end
