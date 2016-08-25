4bb736ae829233c06c21c615ab0321d221ed1a85
Marta <martafarreribas@gmail.com>
2016-08-25 15:54:21 +0200

    updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 	has_many :orders
+	has_many :comments
 end
