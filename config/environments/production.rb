d0a2ad1ab73fb6ca6e4d54a26d8a87cc9861886e
Marta <martafarreribas@gmail.com>
2016-08-31 17:48:56 +0200

    6.13 Full

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.web_socket_server_url = "wss://stormy-spire-55602.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://stormy-spire-55602.herokuapp.com', 'http://stormy-spire-55602.herokuapp.com']
 end
