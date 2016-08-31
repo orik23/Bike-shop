b35e12b55bfbba1251a80e7010d71fdd877ca4b2
Marta <martafarreribas@gmail.com>
2016-08-31 19:38:20 +0200

    6.14

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,2 +1,12 @@
 class ApplicationJob < ActiveJob::Base
+	queue_as :default
+
+  def perform(comment, current_user)
+    ProductChannel.broadcast_to comment.product_id, comment: render_comment(comment, current_user)
+  end
+
+  private
+    def render_comment(comment, current_user)
+      CommentsController.render(partial: 'comments/comment', locals: {comment: comment, current_user: current_user })	
+    end
 end
